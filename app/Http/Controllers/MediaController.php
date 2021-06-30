e77ff7b724495ee66b0ce4c77b397feec9038877
Alemoh Rapheal <raphealenike@gmail.com>
2021-06-30 14:10:11 -0700

    Fixed Display Media Form

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -9,35 +9,36 @@ use JD\Cloudder\Facades\Cloudder;
 
 class MediaController extends Controller
 {
-    public function media(Request $request){
-        if($request->isMethod('post')) {
-            $user = Auth::user();
-            if($user){
-                $this->validate($request, [
-                    'avatar' => 'image|mimes:jpeg,png,jpg|max:1048|required',
-                ]);
-                if ($request->hasFile('avatar')) {
+    public function media(Request $request)
+    {
+        if ($request->isMethod('post')) {
+            $this->validate($request, [
+                'avatar' => 'image|mimes:jpeg,png,jpg|max:1048|required',
+            ]);
 
-                    $image_name = $request->file('avatar')->getRealPath();
-                    Cloudder::upload($image_name, null, array("folder" => "laravel_tutorial",  "overwrite" => FALSE,
-                    "resource_type" => "image", "responsive" => TRUE, "transformation" => array("quality"=>"70", "width"=>"250", "height" => "250", "crop"=>"scale" )) );
-                    $public_id = Cloudder::getPublicId();
-                    $width = 250;   $height = 250;
-                    $image_url = Cloudder::show(Cloudder::getPublicId(), ["width" => $width, "height"=>$height, "crop"=> "scale", "quality" => 70, "secure"=>"true"]);
+            $image_name = $request->file('avatar')->getRealPath();
+            Cloudder::upload($image_name, null, array(
+                "folder" => "laravel_tutorial",  "overwrite" => FALSE,
+                "resource_type" => "image", "responsive" => TRUE, "transformation" => array("quality" => "70", "width" => "250", "height" => "250", "crop" => "scale")
+            ));
 
-                    if($public_id != null){
-                        $image_public_id_exist = User::select('public_id')->where('id', Auth::user()->id)->get();
-                       Cloudder::delete($image_public_id_exist);
-                    }
-                    $user = User::find($user->id);
-                    $user->public_id = $public_id;
-                    $user->avatar_url = $image_url;
-                    $user->update();
-                    return back()->with('success_msg', 'Media successfully updated!');
-                    }
-                }
-            }else{
-                return view('media/upload');
+            $public_id = Cloudder::getPublicId();
+            $width = 250;
+            $height = 250;
+            $image_url = Cloudder::show(Cloudder::getPublicId(), ["width" => $width, "height" => $height, "crop" => "scale", "quality" => 70, "secure" => "true"]);
+
+            if ($public_id != null) {
+                $image_public_id_exist = User::select('public_id')->where('id', Auth::user()->id)->get();
+                Cloudder::delete($image_public_id_exist);
             }
+            
+            $user = User::find(Auth::user()->id);
+            $user->public_id = $public_id;
+            $user->avatar_url = $image_url;
+            $user->update();
+            return back()->with('success_msg', 'Media successfully updated!');
+        } else {
+            return view('media');
         }
+    }
 }
